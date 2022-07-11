490e7b2d0899f5c74872390b5543326208b0e14c
quang11103 <dominhquang11103@gmail.com>
2022-07-11 10:20:09 +0700

    ok

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace thuchanhcsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quanlysanpham quanlysanpham = new Quanlysanpham();
+
+            while (true)
+            {
+                Console.WriteLine("\nCHUONG TRINH QUAN LY SINH VIEN C#");
+                Console.WriteLine("MENU");
+                Console.WriteLine("1. Them san pham.");
+                Console.WriteLine("2. Hien thi san pham.");
+                Console.WriteLine("3. Xoa san pham.");
+                Console.WriteLine("4. Thoat.");
+
+                Console.Write("Nhap tuy chon: ");
+                int key = Convert.ToInt32(Console.ReadLine());
+                switch (key)
+                {
+                    case 1:
+                        Console.WriteLine("\n1. Them san pham.");
+                        quanlysanpham.Nhapsanpham();
+                        Console.WriteLine("\nThem san pham thanh cong!");
+                        break;
+                    case 2:
+                        if (quanlysanpham.SoLuongSanPham() > 0)
+                        {
+                            Console.WriteLine("\nHien thi danh sach san pham");
+                            quanlysanpham.ShowSanPham(quanlysanpham.getListSanPham());
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nkhong co san pham trong danh sach!");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("\nxóa sản phẩm");
+                        quanlysanpham.XoaSanPhamm();
+                        Console.WriteLine("\nxóa sản phẩm thành công!");
+                        break;
+                    case 4:
+                        Console.WriteLine("\nBan da chon thoat chuong trinh!");
+                        return;
+                    default:
+                        Console.WriteLine("\nKhong co chuc nang nay!");
+                        Console.WriteLine("\nHay chon chuc nang trong hop menu.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Quanlysanpham.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace thuchanhcsharp
+{
+    class Quanlysanpham
+    {
+        private List<Sanpham> Listsanpham = null;
+
+        public Quanlysanpham()
+        {
+            Listsanpham = new List<Sanpham>();
+        }
+
+        public int SoLuongSanPham()
+        {
+            int Count = 0;
+            if (Listsanpham != null)
+            {
+                Count = Listsanpham.Count;
+            }
+            return Count;
+        }
+
+        public void Nhapsanpham()
+        {
+            Sanpham sp = new Sanpham();
+
+            Console.Write("Nhap id san pham: ");
+            sp.ID = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Nhap ten san pham: ");
+            sp.Name = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Nhap gia san pham ");
+            sp.Price = Convert.ToString(Console.ReadLine());
+
+            Listsanpham.Add(sp);
+
+        }
+        public void ShowSanPham(List<Sanpham> listSP)
+        {
+            // hien thi tieu de cot
+            Console.WriteLine("{0, -5} {1, -20} {2, -5} ",
+                  "ID", "Name", "Price");
+            // hien thi danh sach sinh vien
+            if (listSP != null && listSP.Count > 0)
+            {
+                foreach (Sanpham sp in listSP)
+                {
+                    Console.WriteLine("{0, -5} {1, -20} {2, -5} ",
+                                      sp.ID, sp.Name, sp.Price);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public void XoaSanPhamm()
+        {
+            Console.WriteLine("ID sản phẩm cần xóa là");
+            Sanpham sp = new Sanpham();
+            sp.ID = Convert.ToString(Console.ReadLine());
+            Listsanpham.Remove(sp);
+        }
+        public List<Sanpham> getListSanPham()
+        {
+            return Listsanpham;
+        }
+
+
+    }
+}
